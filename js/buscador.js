1fd559ed49a2587203671575838be2b041c2b1c2
Camilo Montecino <camilo.montecino.a@gmail.com>
2022-12-06 08:47:55 -0300

    Uso de localStorage

--- js/buscador.js
+++ js/buscador.js
@@ -1,40 +1,58 @@
+//3ra PreEntrega Busqueda de Pedido
+const botonConsulta = document.getElementById("botonConsulta");
+const resultado = document.getElementById("resultado");
 
-// const boton = document.getElementById("botonConsulta")
-// const consultaDOM = parseInt(document.getElementById("buscaPedido"));
-// const resultado = document.getElementById("resultado");
-// const ind = 0;
+function buscarPedido(consultaDOM) {
 
-// function buscarPedido(){
-//     const pedidoEncontrado = listaPedidos.find(pedido => pedido.numPedido == consultaDOM)
-//     const existePedido = listaPedidos.some(pedido => pedido.numPedido == consultaDOM);
-//     // localStorage.setItem('pedidoConsultado', JSON.stringify(pedidoEncontrado))
-//     return existePedido
-// }
+    //Testers 
 
+    // largoLista = listaPedidos.length;
+    // console.log("La cantidad total de pedidos en la memoria es de: ");
+    // console.log(largoLista);
 
-// boton.onclick=()=>{
+    // for (let i = 0; i < largoLista; i++) { //Pendiente cambio a for of...
 
-//     buscarPedido();
-//     const ind= consultaDOM - 1;
-//     console.log(existePedido);
-//     // console.log(pedidoEncontrado)
-    
-//     // console.log(pedidoEncontrado);
-    
-// //     buscarPedido() ?
+    //     console.log("Información de la cotización n°", listaPedidos[i].numPedido);
+    //     console.log("La cantidad de personas:", listaPedidos[i].cantidad);
+    //     console.log("El tipo de servicio:", listaPedidos[i].servicio);
+    //     console.log("Costo:", listaPedidos[i].costo);
 
-// //     resultado.innerHTML += `<p> Su pedido es el siguiente: N° de pedido:${listaPedidos[indice].numPedido} </p> 
-// //     <br> <p> Cantidad de personas: ${listaPedidos[indice].cantidad} </p>
-// //     <br> <p> Tipo de Servicio: ${listaPedidos[indice].servicio} </p>
-// //     <br> <p> Cantidad de personas: ${listaPedidos[indice].costo} </p>
-// //     `
-   
-// //     :
-// //    resultado.innerHTML="Su pedido no ha sido encontrado en la memoria";
-   
-// }
+    //     console.log("\n"); //Salto de línea
+
+    // }
+
+    //Funcionalidad de la Funcion
+    let existePedido = listaPedidos.some(pedido => pedido.numPedido == consultaDOM);
+    return existePedido;
+
+}
 
 
+botonConsulta.onclick = () => {
+    let consultaDOM = parseInt(document.getElementById("buscaPedido").value);
+    console.log(consultaDOM); //Verificacion de variable
+    largoLista = listaPedidos.length;
 
+    if (consultaDOM > 0 && consultaDOM <= largoLista) {
+        buscarPedido(consultaDOM);
 
-// const existePedido = listaPedidos.some(pedido => pedido.numPedido == consulta);
+        if (buscarPedido(consultaDOM)) {
+            let pedidoEncontrado = listaPedidos.find(pedido => pedido.numPedido == consultaDOM);
+            console.log(pedidoEncontrado);
+
+            let j = consultaDOM - 1;
+
+            (pedidoEncontrado) ? resultado.innerHTML += `<hr><p> Su pedido es el siguiente: N° de pedido:${listaPedidos[j].numPedido}</p>
+    <br> <p> Cantidad de personas: ${listaPedidos[j].cantidad} </p>
+    <br> <p> Tipo de Servicio: ${listaPedidos[j].servicio} </p>
+    <br> <p> Cantidad de personas: ${listaPedidos[j].costo} </p>
+    `: resultado.innerHTML += "<hr>Su pedido no ha sido encontrado en la memoria";
+        }
+    } else {
+        alert("El numero de la consulta del pedido no corresponde a ningún numero en sistema"); // Se mejorará con DOM
+        console.log(pedidoEncontrado);
+        console.log(existePedido);
+        console.log(consultaDOM);
+    }
+
+}
--- js/javascript.js
+++ js/javascript.js
@@ -93,6 +93,11 @@ botonFormulario.onclick = () => {
 
     ingresarNuevoPedido(email, cantidadPersonas, tipoDeServicio);
 
+    const guardarLocal = (clave, valor) => {
+        localStorage.setItem(clave, valor)
+    };
+    guardarLocal("listaPedidosGuardada", JSON.stringify(listaPedidos));
+
     console.log("\n");
     console.table(listaPedidos);
     console.log("\n");
@@ -101,73 +106,3 @@ botonFormulario.onclick = () => {
 }
 
 
-// while (cantidadPersonas >= 300 || tipoDeServicio <= 0) {
-//     alert();
-// }
-
-
-//3ra PreEntrega Busqueda de Pedido
-
-
-
-const botonConsulta = document.getElementById("botonConsulta");
-const resultado = document.getElementById("resultado");
-// let pedidoEncontrado = [];
-// let existePedido = false;
-
-function buscarPedido(consultaDOM) {
-
-    //Testers 
-    largoLista = listaPedidos.length;
-    console.log("La cantidad total de pedidos en la memoria es de: ");
-    console.log(largoLista);
-
-    for (let i = 0; i < largoLista; i++) { //Pendiente cambio a for of...
-
-        console.log("Información de la cotización n°", listaPedidos[i].numPedido);
-        console.log("La cantidad de personas:", listaPedidos[i].cantidad);
-        console.log("El tipo de servicio:", listaPedidos[i].servicio);
-        console.log("Costo:", listaPedidos[i].costo);
-
-        console.log("\n"); //Salto de línea
-
-    }
-
-    //Funcionalidad de la funcion
-    let existePedido = listaPedidos.some(pedido => pedido.numPedido == consultaDOM);
-    // console.log(pedidoEncontrado);
-    // console.log(existePedido);
-    // console.log(consultaDOM);
-    return existePedido;
-
-}
-
-
-botonConsulta.onclick = () => {
-    let consultaDOM = parseInt(document.getElementById("buscaPedido").value);
-    console.log(consultaDOM);
-    largoLista = listaPedidos.length;
-
-    if (consultaDOM > 0 && consultaDOM <= largoLista) {
-        buscarPedido(consultaDOM);
-
-        if (buscarPedido(consultaDOM)) {
-            let pedidoEncontrado = listaPedidos.find(pedido => pedido.numPedido == consultaDOM);
-            console.log(pedidoEncontrado);
-
-            let j = consultaDOM - 1;
-
-            (pedidoEncontrado) ? resultado.innerHTML += `<hr><p> Su pedido es el siguiente: N° de pedido:${listaPedidos[j].numPedido}</p>
-    <br> <p> Cantidad de personas: ${listaPedidos[j].cantidad} </p>
-    <br> <p> Tipo de Servicio: ${listaPedidos[j].servicio} </p>
-    <br> <p> Cantidad de personas: ${listaPedidos[j].costo} </p>
-    `: resultado.innerHTML += "<hr>Su pedido no ha sido encontrado en la memoria";
-        }
-    } else {
-        alert("El numero de la consulta del pedido no corresponde a ningún numero en sistema"); // Se mejorará con DOM
-        console.log(pedidoEncontrado);
-        console.log(existePedido);
-        console.log(consultaDOM);
-    }
-
-}
